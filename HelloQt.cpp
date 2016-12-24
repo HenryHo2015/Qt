849cbad8838b60b59940c79d4a448e339fafe926
HenryHo2015 <hxl2000@gmail.com>
2016-12-23 18:26:40 -0600

    Update HelloQt.cpp

--- HelloQt.cpp
+++ HelloQt.cpp
@@ -1,13 +1,14 @@
 #include <QApplication>
 #include <QLabel>
 
-//My first Qt app
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
-        QApplication app(argc,argv);
-        QLabel *label * new QLabel("Hello Qt");
-        label->show();
-        return app.exec();
+      QApplication app(argc, argv);
+
+      QLabel label("Hello Qt!");
+      label.show();
+
+     return app.exec();
 }
 
 //("<h1><font.color = red>Hello </font> <br>Qt</br></h1>");
